d47ed727fddc8b024a472f8e1d4a2f95c306bb49
kudlicka <kudlicka@e8d9f1e1-c734-4741-8af1-5c470c81a80d>
2016-09-21 19:55:11 +0000

    * instance ImmersiveShell Qt way

--- source/virtualdesktopmanager/virtualdesktopmanagerinternal.cpp
+++ source/virtualdesktopmanager/virtualdesktopmanagerinternal.cpp
@@ -1,13 +1,16 @@
 #include "virtualdesktopmanagerinternal.h"
 
-#include "virtualdesktopmanagerinternal/immersiveshell.h"
+#include <ActiveQt/QAxObject>
+#include <QtCore/QUuid>
 #include "virtualdesktopmanagerinternal/virtualdesktopapiunknown.h"
 
 VirtualDesktopManagerInternal::VirtualDesktopManagerInternal()
 {
+  QAxObject immersiveShell("c2f03a33-21f5-47fa-b4bb-156362a2f239");
+
   _COM_SMARTPTR_TYPEDEF(IServiceProvider, IID_IServiceProvider);
   IServiceProvider *serviceProvider = nullptr;
-  CoCreateInstance(CLSID_ImmersiveShell, nullptr, CLSCTX_SERVER, IID_IServiceProvider, reinterpret_cast<LPVOID *>(&serviceProvider));
+  immersiveShell.queryInterface("6d5140c1-7436-11ce-8034-00aa006009fa", reinterpret_cast<LPVOID *>(&serviceProvider));
 
   serviceProvider->QueryService(CLSID_VirtualDesktopAPI_Unknown, IID_IVirtualDesktopManagerInternal, reinterpret_cast<LPVOID *>(&_iVirtualDesktopManagerInternal));
 }
