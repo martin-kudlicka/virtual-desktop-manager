32408aaa09746ff2f838a487cc03af3d19e57924
kudlicka <kudlicka@e8d9f1e1-c734-4741-8af1-5c470c81a80d>
2016-09-22 20:36:06 +0000

    + hotkey sequence edits

--- source/optionsdialog.cpp
+++ source/optionsdialog.cpp
@@ -1,6 +1,7 @@
 #include "optionsdialog.h"
 
 #include "options.h"
+#include <QtWidgets/QKeySequenceEdit>
 
 OptionsDialog::OptionsDialog(QWidget *parent /* Q_NULLPTR */) : QDialog(parent), _widgetSettings(gOptions.data())
 {
@@ -9,6 +10,20 @@ OptionsDialog::OptionsDialog(QWidget *parent /* Q_NULLPTR */) : QDialog(parent),
   setupSettings();
 
   _widgetSettings.load();
+
+  setupHotkeys();
+}
+
+void OptionsDialog::setupHotkeys() const
+{
+  auto formLayout = qobject_cast<QFormLayout *>(_ui.hotkeysContents->layout());
+
+  for (auto index = 0; index < _ui.desktopCount->value(); index++)
+  {
+    auto hotkeyEdit = new QKeySequenceEdit(_ui.hotkeysContents);
+
+    formLayout->addRow(QString("Desktop %1").arg(index), hotkeyEdit);
+  }
 }
 
 void OptionsDialog::setupSettings()
