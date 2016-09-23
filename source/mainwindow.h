ee8ea54773c065650519a5a0c294b7fd69e31437
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2016-09-23 17:22:57 +0000

    + un/register system hotkey

--- source/mainwindow.h
+++ source/mainwindow.h
@@ -11,14 +11,17 @@ class MainWindow : public QMainWindow
 
   public:
              MainWindow();
-    virtual ~MainWindow();
+    virtual ~MainWindow() Q_DECL_OVERRIDE;
 
   private:
     ApplicationModel _applicationModel;
     AppWindows       _appWindows;
     Ui::MainWindow   _ui;
 
+    void registerHotkeys      () const;
     void setupApplicationModel();
+
+    virtual bool nativeEvent(const QByteArray &eventType, void *message, long *result) Q_DECL_OVERRIDE;
 
   private slots:
     void on_actionOptions_triggered(bool checked = false);
